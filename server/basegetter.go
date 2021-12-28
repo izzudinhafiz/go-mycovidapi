20e3fdab323d1f0ae56ffd2f918f053fff2d0711
izzudinhafiz <izzudin.hafiz@gmail.com>
2021-12-28 22:42:25 +0800

    Added validation response and handled different date combination

--- server/basegetter.go
+++ server/basegetter.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"errors"
+	"net/http"
 	"reflect"
 	"strings"
 	"time"
@@ -29,23 +30,25 @@ var statesAbrvMap = map[string]string {
 	"PNG": "Pulau Pinang",
 }
 
+var (
+	ErrorNoState = errors.New("requires at least one state")
+	ErrorUnknownState = errors.New("unknown state(s) passed as param")
+)
+
 func parseStates(states_str string) ([]string, error) {
 	if states_str == "" {
-		return []string{}, errors.New("requires at least one state")
+		return []string{}, ErrorNoState
 	}
 
 	states_raw := strings.Split(states_str, ",")
-	states := []string{}
 	for _, state_raw := range states_raw {
-		state, key_exists := statesAbrvMap[state_raw]
-		if key_exists {
-			states = append(states, state)
-		} else {
-			return []string{}, errors.New("unknown state(s) passed as param")
+		_, key_exists := statesAbrvMap[strings.ToUpper(state_raw)]
+		if !key_exists {
+			return []string{}, ErrorUnknownState
 		}
 	}
 
-	return states, nil
+	return states_raw, nil
 }
 
 type CountryRequests struct {
@@ -56,21 +59,37 @@ type CountryRequests struct {
 type StateRequests struct {
 	StartDate time.Time `form:"start_date" time_format:"2006-01-02"`
 	EndDate time.Time `form:"end_date" time_format:"2006-01-02"`
-	State string
+	State string `form:"state_id"`
+}
+
+type ValidationError struct {
+	ErrorMessage string `json:"msg"`
+	Location string `json:"loc"`
+	Type string `json:"type"`
 }
 
 func (s *Server) getCountry(c *gin.Context, out interface{}) error {
 	var query CountryRequests
 	c.BindQuery(&query)
 
-	//TODO: Handle different combination of start_date, end_date
 	val := reflect.ValueOf(out).Interface()
-	tx := s.DB.Where("date BETWEEN ? AND ?", query.StartDate, query.EndDate).Find(&val, "state ='MY'")
+	tx := s.DB
+	if query.StartDate.IsZero() && query.EndDate.IsZero() {
+	} else if !query.StartDate.IsZero() && query.EndDate.IsZero() {
+		tx = s.DB.Where("date >= ?", query.StartDate)
+	} else if query.StartDate.IsZero() && !query.EndDate.IsZero() {
+		tx = s.DB.Where("date <= ?", query.EndDate)
+	} else {
+		tx = s.DB.Where("date BETWEEN ? AND ?", query.StartDate, query.EndDate)
+	}
+
+	tx = tx.Order("date").Find(&val, "state ='MY'")
 	if tx.Error != nil {
 		log.Errorf("%v?%v: %v",c.Request.URL.Path, c.Request.URL.Query(), tx.Error)
+		c.AbortWithStatus(http.StatusInternalServerError)
 		return tx.Error
 	}
-	c.JSON(200, val)
+	c.JSON(http.StatusOK, val)
 	return nil
 }
 
@@ -81,15 +100,41 @@ func (s *Server) getState(c *gin.Context, out interface{}) error {
 	states, err := parseStates(query.State)
 	if err != nil {
 		log.Errorf("%v?%v: %v", c.Request.URL.Path, c.Request.URL.Query(), err)
+		errorMsg := ValidationError{ErrorMessage: "Validation Error",}
+		if err == ErrorNoState {
+			errorMsg = ValidationError{
+				ErrorMessage: "requires at least one state",
+				Location: "state_id",
+				Type: "string (comma separated)",
+			}
+		} else if err == ErrorUnknownState{
+			errorMsg = ValidationError{
+				ErrorMessage: "invalid states in query parameter",
+				Location: "state_id",
+				Type: "string (comma separated)",
+			}
+		}
+		c.AbortWithStatusJSON(http.StatusBadRequest, errorMsg)
 		return err
 	}
 
 	val := reflect.ValueOf(out).Interface()
-	tx := s.DB.Where("date BETWEEN ? AND ?", query.StartDate, query.EndDate).Where("state IN ?", states).Find(&val)
+	tx := s.DB
+	if query.StartDate.IsZero() && query.EndDate.IsZero() {
+	} else if !query.StartDate.IsZero() && query.EndDate.IsZero() {
+		tx = s.DB.Where("date >= ?", query.StartDate)
+	} else if query.StartDate.IsZero() && !query.EndDate.IsZero() {
+		tx = s.DB.Where("date <= ?", query.EndDate)
+	} else {
+		tx = s.DB.Where("date BETWEEN ? AND ?", query.StartDate, query.EndDate)
+	}
+
+	tx = tx.Where("state IN ?", states).Order("date, state").Find(&val)
 	if tx.Error != nil {
 		log.Errorf("%v?%v: %v",c.Request.URL.Path, c.Request.URL.Query(), tx.Error)
+		c.AbortWithStatus(http.StatusInternalServerError)
 		return tx.Error
 	}
-	c.JSON(200, val)
+	c.JSON(http.StatusOK, val)
 	return nil
 }
