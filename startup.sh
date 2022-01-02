f930223a71e1a96259c83f7d1c7fe5a2653d3b92
izzudinhafiz <izzudin.hafiz@gmail.com>
2022-01-02 20:32:20 +0800

    added grace period in startup to allow DB init

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 crond # starts the cron daemon in the background so we can run ingest every few hours
+sleep 1m
 /serverapi ingest # performs an ingest
 /serverapi serve # Runs the server
