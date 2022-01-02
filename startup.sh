3fae9233d179c19c8d7fdf615be1b0bd8482a1ab
izzudinhafiz <izzudin.hafiz@gmail.com>
2022-01-02 20:33:03 +0800

    --amend

--- startup.sh
+++ startup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 crond # starts the cron daemon in the background so we can run ingest every few hours
+echo "Waiting 1 minute for DB initialization"
 sleep 1m
 /serverapi ingest # performs an ingest
 /serverapi serve # Runs the server
